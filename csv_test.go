1eacb63242fedfed401885f7071012823a3579a6
Elviie Abdulmennanova <el123abd@gmail.com>
2024-02-16 15:00:44 -0500

    Add table-driven test for statsFunc type functions

--- csv_test.go
+++ csv_test.go
@@ -21,3 +21,33 @@ func TestAvg(t *testing.T) {
 		t.Errorf("Expected %f, got %f instead\n", exp, res)
 	}
 }
+
+// test all operation functions by applying statsFunc type and table-driven testing
+func TestOperations(t *testing.T) {
+	data := [][]float64{
+		{10, 20, 15, 30, 45, 50, 100, 30},
+		{5.5, 8, 2.2, 9.75, 8.45, 3, 2.5, 10.25, 4.75, 6.1, 7.67, 12.287, 5.47},
+		{-10, -20},
+		{102, 37, 44, 57, 67, 129},
+	}
+
+	testCases := []struct {
+		name string
+		op   statsFunc
+		exp  []float64
+	}{
+		{"Sum", sum, []float64{300, 85.927, -30, 436}},
+		{"Avg", avg, []float64{37.5, 6.609769230769231, -15, 72.666666666666666}},
+	}
+
+	for _, tc := range testCases {
+		for i, d := range data {
+			t.Run(tc.name, func(t *testing.T) {
+				res := tc.op(d)
+				if res != tc.exp[i] {
+					t.Errorf("Expected: %v, got: %v", tc.exp[i], res)
+				}
+			})
+		}
+	}
+}
