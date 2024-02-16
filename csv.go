019d8a6964fabb028cc1c48d029b7005e037b038
Elviie Abdulmennanova <el123abd@gmail.com>
2024-02-16 11:11:49 -0500

    Add stat functions: sum and avg

---
+++ csv.go
@@ -0,0 +1,15 @@
+package main
+
+// sum calculates the sum of all values in the column
+func sum(data []float64) float64 {
+	sum := 0.0
+	for _, v := range data {
+		sum += v
+	}
+	return sum
+}
+
+// avg determines avarage value of the column
+func avg(data []float64) float64 {
+	return sum(data) / float64(len(data))
+}
