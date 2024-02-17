64c5008078064f59179304cf9e88240b33b11376
Elviie Abdulmennanova <el123abd@gmail.com>
2024-02-17 10:13:54 -0500

    Add csv2Float func to parse csv data

--- csv.go
+++ csv.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"encoding/csv"
+	"errors"
+	"fmt"
+	"io"
+	"strconv"
+	"testing/iotest"
+)
+
 // statsFunc defines a generic statistical function
 type statsFunc func(data []float64) float64
 
@@ -15,4 +24,42 @@ func sum(data []float64) float64 {
 // avg determines avarage value of the column
 func avg(data []float64) float64 {
 	return sum(data) / float64(len(data))
+}
+
+// cvs2float parses the contents of the csv file into a []float64
+// parses the contents of the specified column only
+func cvs2float(r io.Reader, column int) ([]float64, error) {
+	if column < 1 {
+		return nil, fmt.Errorf("%w", ErrInvalidColumn)
+	}
+
+	// adjusting for a 0 based index
+	column--
+
+	cr := csv.NewReader(r)
+	allData, err := cr.ReadAll()
+	if err != nil {
+		if errors.Is(iotest.ErrTimeout, err) {
+			return nil, iotest.ErrTimeout
+		}
+		return nil, fmt.Errorf("Cannot read from a file: %v", err)
+	}
+	var data []float64
+
+	for i, row := range allData {
+
+		// discard  the title line
+		if i == 0 {
+			continue
+		}
+		if column >= len(row) {
+			return nil, fmt.Errorf("%w: File has only %d columns", ErrInvalidColumn, len(row))
+		}
+		v, err := strconv.ParseFloat(row[column], 64)
+		if err != nil {
+			return nil, fmt.Errorf("%w: %s", ErrNotNumber, err)
+		}
+		data = append(data, v)
+	}
+	return data, nil
 }
